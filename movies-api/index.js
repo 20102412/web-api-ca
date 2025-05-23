371fb37ba8e1af5581e1bf571ee99402f646190c
Amelia Coveney <20102412@mail.wit.ie>
2025-05-23 16:07:20 +0100

    Added movies router; get movies from TMDB

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import usersRouter from './api/users';
 import './db';
 import authenticate from './authenticate';
 import cors from 'cors';
+import moviesRouter from './api/movies';   
 
 
 
@@ -29,6 +30,8 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
+
+app.use('/api/movies', moviesRouter); 
 
 app.use(errHandler);
 
